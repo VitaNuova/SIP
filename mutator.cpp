173857b98da050bd31ff4f4db17a35fef42050da
VitaNuova <vitanuova0512@gmail.com>
2017-05-07 19:54:29 +0200

    attempt to insert one checker

--- mutator.cpp
+++ mutator.cpp
@@ -28,6 +28,133 @@ int main(int argc, const char* argv[]) {
  
    BPatch_image* appImage = (*appbin).getImage();
    precompute_hashes(appImage);
+   
+   vector<BPatch_function*>* functions = (*appImage).getProcedures();
+   int counter =0;     
+   for(BPatch_function* f: *functions) {
+      if((*f).getName() == "main") {
+         cout << "Function name: " << (*f).getName() << endl;
+         set<BPatch_basicBlock*> blocks = get_basic_blocks(f);
+         for(BPatch_basicBlock* bb: blocks) {
+	    if(counter == 0) {
+               vector<BPatch_point*>* points;
+               vector<BPatch_function*> functions;
+               (*appImage).findFunction("main", functions);
+               points = (*functions[0]).findPoint(BPatch_locEntry);    
+               //unsigned long start = 0x400666;
+	       //unsigned long end = 0x4006c7 call to time;
+	       BPatch_variableExpr* start = (*appbin).malloc(*((*appImage).findType("unsigned long")));
+	       BPatch_variableExpr* end = (*appbin).malloc(*((*appImage).findType("unsigned long")));
+	       BPatch_variableExpr* hash_sum = (*appbin).malloc(*((*appImage).findType("unsigned long")));
+
+	       //BPatch_constExpr* startConst = new BPatch_constExpr((*bb).getStartAddress());
+               //BPatch_constExpr* endConst = new BPatch_constExpr((*bb).getEndAddress());
+	       BPatch_constExpr* startConst = new BPatch_constExpr(0x400666);
+               BPatch_constExpr* endConst = new BPatch_constExpr(0x4006c7);
+	       BPatch_constExpr* hashConst = new BPatch_constExpr(0);
+	      
+	       BPatch_arithExpr* assignStart = new BPatch_arithExpr(BPatch_assign, *start, *startConst);
+	       BPatch_arithExpr* assignEnd = new BPatch_arithExpr(BPatch_assign, *end, *endConst);
+	       BPatch_arithExpr* assignInitialHash = new BPatch_arithExpr(BPatch_assign, *hash_sum, *hashConst);
+	      
+               BPatch_boolExpr* condition = new BPatch_boolExpr(BPatch_ne, *start, *end);
+	       //BPatch_constExpr* valueAtAddr = new BPatch_constExpr(*(unsigned char*)((*bb).getStartAddress()));
+	       BPatch_constExpr* valueAtAddr = new BPatch_constExpr(*((unsigned char*)(0x400666)));  BPatch_arithExpr* sumHash = new BPatch_arithExpr(BPatch_plus, *hash_sum, *valueAtAddr);
+	       BPatch_arithExpr* assignHash = new BPatch_arithExpr(BPatch_assign, *hash_sum, *sumHash);
+	       BPatch_constExpr* oneConst = new BPatch_constExpr(1);
+               BPatch_arithExpr* incrementStart = new BPatch_arithExpr(BPatch_plus, *start, *oneConst);
+	       BPatch_arithExpr* assignNewStart = new BPatch_arithExpr(BPatch_assign, *start, *incrementStart);
+               vector<BPatch_snippet*> whileItems;
+	       whileItems.push_back(sumHash);
+               whileItems.push_back(assignHash);
+               whileItems.push_back(incrementStart);
+               whileItems.push_back(assignNewStart);
+               BPatch_sequence* whileSequence = new BPatch_sequence(whileItems);
+	       BPatch_whileExpr* finalWhile = new BPatch_whileExpr(*condition, *whileSequence);
+               
+               BPatch_constExpr* expectedHash = new BPatch_constExpr(0x21d4);
+	       BPatch_boolExpr* ifCondition = new BPatch_boolExpr(BPatch_ne, *hash_sum, *expectedHash);
+               /*vector<BPatch_snippet*> argToExit;
+               argToExit.push_back(oneConst);               
+               vector<BPatch_function*> exitFuncs;
+               (*appImage).findFunction("exit", exitFuncs);
+               if(exitFuncs.size() == 0) {
+                  fprintf(stderr, "Could not find exit");
+		    
+               }
+               BPatch_funcCallExpr* exitCall = new BPatch_funcCallExpr(*(exitFuncs[0]), argToExit);*/
+               BPatch_constExpr* argToPrintf = new BPatch_constExpr("Fail\n");
+               vector<BPatch_snippet*> argsToPrintf;
+               argsToPrintf.push_back(argToPrintf);
+               vector<BPatch_function*> printfFuncs;
+               (*appImage).findFunction("printf", printfFuncs, true, true, true);
+               if(printfFuncs.size() == 0) {
+                  fprintf(stderr, "Could not find printf");
+		  exit(0);    
+               }
+               BPatch_funcCallExpr* printfCall = new BPatch_funcCallExpr(*(printfFuncs[0]), argsToPrintf);
+               vector<BPatch_snippet*> ifBody;
+               //ifBody.push_back(exitCall);
+               ifBody.push_back(printfCall);
+               BPatch_sequence* ifBodySeq = new BPatch_sequence(ifBody);
+               BPatch_ifExpr* ifExpr = new BPatch_ifExpr(*ifCondition, *ifBodySeq); 
+               
+               vector<BPatch_snippet*> wholeSnippet;
+               wholeSnippet.push_back(assignStart);
+               wholeSnippet.push_back(assignEnd);
+               wholeSnippet.push_back(assignInitialHash);
+               wholeSnippet.push_back(finalWhile);
+               wholeSnippet.push_back(ifExpr);
+               
+	       BPatch_sequence* wholeSeq = new BPatch_sequence(wholeSnippet);
+               
+               (*appbin).insertSnippet(*wholeSeq, *points);
+	       /*(*appbin).free(*start);
+	       (*appbin).free(*end);
+	       (*appbin).free(*hash_sum);
+               delete startConst;
+	       delete endConst;
+               delete hashConst;
+               delete assignStart;
+               delete assignEnd;
+               delete assignInitialHash;
+               delete condition;
+               delete valueAtAddr;
+               delete sumHash;
+               delete assignHash;
+               delete oneConst;
+               delete incrementStart;
+               delete assignNewStart;
+               delete expectedHash;
+               delete ifCondition;
+               delete exitCall;
+               delete argToPrintf;
+               delete printfCall;
+               delete ifBodySeq;
+               delete finalWhile;
+               delete ifExpr;
+               delete wholeSeq;
+               delete whileSequence;*/
+	       //BPatch_point* point = (*bb).findEntryPoint();
+	       /*
+	       unsigned long start = (*bb).getStartAddress();
+               unsigned long end = (*bb).getEndAddress();
+               unsigned int hash_sum = 0;
+	       printf("start: %lx\n", start);
+	       printf("end: %lx\n", end);*/
+	       /*while(start != end) {
+	          hash_sum += *start;
+	          start++;
+	       }
+	       if(hash_sum != 0x6ae) {
+	          exit(1);
+	       }
+               goto end;*/
+	       counter++;
+	    }
+         }
+      }
+   }
 
    string newpath(path);
    newpath += "_instrumented";
@@ -39,13 +166,18 @@ int main(int argc, const char* argv[]) {
    return 0;
 }
 
+set<BPatch_basicBlock*> get_basic_blocks(BPatch_function* f) { 
+   BPatch_flowGraph* fg = (*f).getCFG();
+   set<BPatch_basicBlock*> blocks;   
+   (*fg).getAllBasicBlocks(blocks);
+return blocks;
+}
+
 void precompute_hashes(BPatch_image* appImage) {   
-   vector<BPatch_function*>* functions = (*appImage).getProcedures();
+   vector<BPatch_function*>* functions = (*appImage).getProcedures();     
    for(BPatch_function* f: *functions) {
       cout << "Function name: " << (*f).getName() << endl;
-      BPatch_flowGraph* fg = (*f).getCFG();
-      set<BPatch_basicBlock*> blocks;
-      (*fg).getAllBasicBlocks(blocks);
+      set<BPatch_basicBlock*> blocks = get_basic_blocks(f);
       unsigned int hashsum_bb = 0;
       for(BPatch_basicBlock* bb: blocks) {
          hashsum_bb = hash_sum(bb);
--- mutator.h
+++ mutator.h
@@ -1,6 +1,8 @@
 #include "BPatch.h"
 #include "BPatch_function.h"
 #include "BPatch_flowGraph.h"
+#include "BPatch_point.h"
 
 unsigned int hash_sum(BPatch_basicBlock*);
 void precompute_hashes(BPatch_image*);
+std::set<BPatch_basicBlock*> get_basic_blocks(BPatch_function*);
